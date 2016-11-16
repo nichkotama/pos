b7eaada460408c422bc2c78c6866c624cb54cd59
Nicholas Kotama <nicholas.kotama@coderscolony.com>
2016-11-16 20:36:57 +0700

    gabungin sama yang jesi

--- sales/add_to_cart.php
+++ sales/add_to_cart.php
@@ -3,8 +3,12 @@ require_once('../php/modular/koneksi.php');
 require_once('../php/modular/otentifikasi.php'); 
 try{
 	$bcode = $_GET['whatever'];
-
-	$query = $db->prepare("SELECT * FROM barang WHERE barcode_barang = '" . $_GET['whatever'] . "'");
+	$metode = $_GET['string']; //number = true; string = false; buat detect dia input nama apa barcode
+	if($metode == "false"){
+		$query = $db->prepare("SELECT * FROM barang WHERE nama_barang = '" . $bcode . "'");
+	}else{
+		$query = $db->prepare("SELECT * FROM barang WHERE barcode_barang = '" . $bcode . "'");
+	}
 	$query->execute();
 	$data = $query->fetch();
 	while ($row = $query->fetchAll(PDO::FETCH_ASSOC))
@@ -12,7 +16,6 @@ try{
 		$data[] = $row;
 	}
 	echo json_encode($data);
-
 }catch(Exception $e){
 	echo $e->getMessage();
 }

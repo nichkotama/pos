cf06b18574281b9602d3e589f5fcc8ce8f5795a8
Alvinjuvianto <alvinjuvianto@gmail.com>
2016-11-18 12:55:33 +0700

    folder purchase

--- php/modular/config.php
+++ php/modular/config.php
@@ -1,6 +1,6 @@
 <?php
 	// general
-	$url_web = 'http://belajar.abc/tugas_bps/';
+	$url_web = 'http://localhost/tugas_bps/';
 	$judul = 'BM Mart POS';
 	$author = 'Kelompok 2';
 	$mode_debug = true;
@@ -8,7 +8,7 @@
 	// database
 	define('db_server', 'localhost'); // Server database, default : localhost
 	define('db_user', 'root'); // Nama user database
-	define('db_pass', 'nicholaskotama'); // Password user database
+	define('db_pass', ''); // Password user database
 	define('db_name', 'supermarket'); // Nama database
 
 	// enkripsi
